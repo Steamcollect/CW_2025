714e2b219f295babcfd63cdb834eed98c469d60c
Steamcollect <eliot.garnier.080805@gmail.com>
2025-01-30 11:31:18 +0100

    Feat : starting main menu

---
+++ Assets/App/Scripts/Wrapper/RSE/UI/RSE_DisableWindow.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_DisableWindow", menuName = "RSE/UI/RSE_DisableWindow")]
+public class RSE_DisableWindow : BT.ScriptablesObject.RuntimeScriptableEvent<string>{}
---
+++ Assets/App/Scripts/Wrapper/RSE/UI/RSE_EnableWindow.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_EnableWindow", menuName = "RSE/UI/RSE_EnableWindow")]
+public class RSE_EnableWindow : BT.ScriptablesObject.RuntimeScriptableEvent<string>{}
