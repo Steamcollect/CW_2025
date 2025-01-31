09ff9d49ea8cb24a4245c44297a447bac7070de3
Steamcollect <eliot.garnier.080805@gmail.com>
2025-01-31 12:27:54 +0100

    Feat : dialog timer

--- Assets/App/Scripts/Wrapper/RSE/UI/RSE_ShowAnswerUI.cs
+++ Assets/App/Scripts/Wrapper/RSE/UI/RSE_ShowAnswerUI.cs
@@ -1,4 +1,4 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "RSE_ShowAnswerUI", menuName = "RSE/_/RSE_ShowAnswerUI")]
-public class RSE_ShowAnswerUI : BT.ScriptablesObject.RuntimeScriptableEvent<SSO_DialogEventData> {}
+public class RSE_ShowAnswerUI : BT.ScriptablesObject.RuntimeScriptableEvent<SSO_DialogEventData, float> {}
